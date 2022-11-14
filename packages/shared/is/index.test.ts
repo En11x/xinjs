250cb4ab912e09f90f5594c9899e51fa3fa85525
pp <pijiaxin@wsdashi.com>
2022-11-14 16:27:37 +0800

    feat: add vitest

---
+++ packages/shared/is/index.test.ts
@@ -0,0 +1,8 @@
+import { describe, expect, it } from 'vitest';
+import { isClient } from '.';
+
+describe('shared/is test', () => {
+  it('the isClient return false', () => {
+    expect(isClient).toEqual(false);
+  });
+});
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { resolve } from 'path';
+import { defineConfig } from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+  },
+  resolve: {
+    alias: {
+      '@xinjs/shared': resolve(__dirname, 'packages/shared/index.ts'),
+    },
+  },
+});
